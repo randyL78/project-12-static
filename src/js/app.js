270c9087692c31c33905235be16eefc0b8956072
RandyL <30959856+randyL78@users.noreply.github.com>
2017-11-17 17:59:58 -0500

    Scroll button functionality added

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,57 @@
 /*jshint esversion: 6 */
 
+
+// wait until html loads
 document.addEventListener('DOMContentLoaded', function () {
 	"use strict";
+	
 
-	const navBar = document.querySelector(".navbar");
-	const main = document.querySelector("body");
+// ************************************************************
+//				Utility Functions
+// ************************************************************	
+	
+	
+	
+	
+	
+// ************************************************************
+//				Navigation
+// ************************************************************
 
-	window.onscroll = function(e) {
-		console.log(navBar.scrollHeight + ", " + e.target.scrollTop);
+	// handle scroll button for portfolio
+	const scrollButton = document.getElementById("button--scroll");
+	const portfolio = document.getElementById("portfolio");
+	let scrolled = false;
+	
+	
+	scrollButton.onclick = () => {
+		if (scrolled) {
+			window.scrollTo(0,0);
+			scrollButton.classList.remove("flip");
+		} else {
+				portfolio.scrollIntoView();		
+				scrollButton.classList.add("flip");
+		}
+		scrolled = !scrolled;
 		
 	};
 	
+	
+	// sticky nav
+	const navBar = document.querySelector(".navbar");
+
+	window.onscroll = function() {
+			console.log(navBar.offsetTop + ", " + portfolio.offsetTop + ", " + window.pageYOffset + ", " + (portfolio.offsetTop - window.getComputedStyle(navBar).height));
+		if (window.pageYOffset >= navBar.offsetTop && !navBar.classList.contains("sticky")) {
+			navBar.classList.add("sticky");
+		} else if (window.pageYOffset <= (portfolio.offsetTop - window.getComputedStyle(navBar).height)) {
+
+		}
+		
+	};
+	
+	
+
 	
 	
 });
