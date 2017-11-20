b9b8355f170f16e8b6471bbd2bd89433564472cb
RandyL <30959856+randyL78@users.noreply.github.com>
2017-11-19 22:52:47 -0500

    Add pop up for project descriptions

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,10 @@
 // wait until html loads
 document.addEventListener('DOMContentLoaded', function () {
 	"use strict";
-	
+// ************************************************************
+//				Utility Functions
+// ************************************************************	
+const imageURL = "./images/";	
 
 // ************************************************************
 //				Utility Functions
@@ -25,6 +28,25 @@ document.addEventListener('DOMContentLoaded', function () {
 	}
 	
 // ************************************************************
+//				Object Constructors
+// ************************************************************		
+	// Create a project object to store important information about each project to display in modal
+	let projectKey = 0;
+	
+	function project(number, title, desription, skills, url) {
+		this.key = projectKey;
+		projectKey ++;
+		this.number = number;
+		this.title = title;
+		this.desription = desription;
+		this.skills = skills;
+		this.url = url;
+		this.image = () => {
+			 return (imageURL + "Project" + this.number + ".png");
+			};
+	}
+	
+// ************************************************************
 //				Navigation
 // ************************************************************
 	// find the computed height for where the nav needs to start "sticking"
@@ -42,10 +64,10 @@ document.addEventListener('DOMContentLoaded', function () {
 	const navBar = document.querySelector("#main-nav");
 	
 	mobileMenu.onclick = () => {
-		if (mobileNav.classList.contains("hidden")) {
-			mobileNav.classList.remove("hidden");
+		if (mobileNav.classList.contains("collapsed")) {
+			mobileNav.classList.remove("collapsed");
 		} else {
-			mobileNav.classList.add("hidden");
+			mobileNav.classList.add("collapsed");
 		}
 	};
 
@@ -109,45 +131,165 @@ document.addEventListener('DOMContentLoaded', function () {
 // ************************************************************
 //				Controller
 // ************************************************************
+	const projectScreen = document.getElementById("project--screen");
+	const numberDisplay = document.getElementById("project--number");
 	const prevButton = document.getElementById("project--prev");
 	const nextButton = document.getElementById("project--next");
-	const numberDisplay = document.getElementById("project--number");
-	const projectScreen = document.getElementById("project--screen");
+	const infoButton = document.getElementById("project--info");
+	const modalOverlay = document.querySelector(".overlay");
 	
-	let projectNumber = parseInt(numberDisplay.textContent);
+	let projectNumber = 0;
 	
+	
+	// =========== Change the display number on the controller and update image in the svg computer screen
 	function setProject() {
-		if (projectNumber > 11) {projectNumber = 1;}
-		if (projectNumber < 1) {projectNumber = 11;}
+		if (projectNumber >= projects.length) {projectNumber = 0;}
+		if (projectNumber < 0) {projectNumber = projects.length - 1;}
 	
-		numberDisplay.textContent = projectNumber;
-		projectScreen.setAttribute("xlink:href", "./images/Project" + projectNumber + ".png");		
+		numberDisplay.textContent = projects[projectNumber].number;
+		let currentImage = projects[projectNumber].image();
+		projectScreen.setAttribute("xlink:href", currentImage);		
 	}
 	
+	
+	// ============ Event handlers
 	// select next project
 	nextButton.onclick = () => {
 		projectNumber++;
 		setProject();
 	};
+	
 	// select previous project
 	prevButton.onclick = () => {
 		projectNumber--;
 		setProject();
 	};	
 	
+	// use a modal to display more information about a project
+	infoButton.onclick = () => {
+		if (modalOverlay.classList.contains("hidden")) {
+			modalOverlay.classList.remove("hidden");
+			addModalContent();
+		}
+	};
 	
 	
+// ************************************************************
+//				Modal
+// ************************************************************
+	// const modalOverlay declared in "Contoller" section to be accessible to controller event handlers
+	const modalContainer = document.querySelector(".modal__container");
+	const modalClose = document.getElementById("modal-close");
+	
+	// ======= add html content to modal
+	function addModalContent() {
+		let skillsHTML = "<ul>";
+		projects[projectNumber].skills.forEach( (currentValue) => {
+			skillsHTML += "<li>" + currentValue + "</li>";
+		});
+		
+		skillsHTML += "</ul>";
+		
+		let modalInner = 
+		`
+			<h2>Project ${projects[projectNumber].number}</h2>
+			<h3>${projects[projectNumber].title}</h3>
+			<p>${projects[projectNumber].desription}</p>
+			<h4>Skills Used:</h4>
+			${skillsHTML}
+			<a href="http://${projects[projectNumber].url}" target="_blank" class="link">Live Demo</a>
+			<hr>
+		`;
+		
+		modalContainer.innerHTML = modalInner;
+	}
 	
 	
-});
+	// ======= prevent user from scrolling page while modal is open
+//	function handleScrollPrevention(e) {
+//		if (!modalOverlay.classList.contains("hidden")) {
+//			e.preventDefault();
+//		}
+//	}
+
+	
+	// close the modal
+	modalClose.onclick = () => {
+		modalOverlay.classList.add("hidden");	
+	};
+	
+	// prevent mouse wheel scrolling with modal open and touchpad scrolling
+//	window.onwheel = (e) => handleScrollPrevention(e);
+//	window.onmousewheel = (e) => handleScrollPrevention(e);
+//	window.ontouchmove = (e) => handleScrollPrevention(e);
+//	
 
 
 
+// ************************************************************
+//				Create project objects
+// ************************************************************
+	const projects = [];
+	
+	projects.push(new project(1, 
+														"Personal Profile",
+													 	"In this project we take a given template and customize placeholder text and edit styles to match with our own goals and personalities. We also learned the power of anchors to link to different areas both in our own website and to other websites.",
+													 	["HTML", "CSS", "Git"],
+													 	"randylayne.com"));
+	projects.push(new project(2, 
+														"Responsive Layout",
+													 	"When preparing to do this project, the main focus of the courses we were taking was a mobile first approach to building a website. Specifically, we learned to format the head of HTML documents for mobile devices and how to use min-width media queries to adjust our layout for different screen sizes.",
+													 	["HTML", "CSS"],
+													 	"randylayne.com/Techdegree-Projects/Responsive-Layout/"));
+	projects.push(new project(3, 
+														"Online Registration Form",
+													 	"This project is all about being able to structure a web form to have a consistent cross-browser look, as well as utilizing HTML5 validation features.",
+													 	["HTML", "CSS"],
+													 	"randylayne.com/Techdegree-Projects/Online-Registration-Form/"));
+	projects.push(new project(4, 
+														"Interactive Photo Gallery",
+													 	"In this project we take a given template and customize placeholder text and edit styles to match with our own goals and personalities",
+													 	["HTML", "CSS", "Javascript", "jQuery", "Plugins"],
+													 	"randylayne.com/Techdegree-Projects/Interactive-Photo-Gallery/"));	
+	projects.push(new project(5, 
+														"Web Style Guide",
+													 	"In this project we take a given template and customize placeholder text and edit styles to match with our own goals and personalities",
+													 	["HTML", "CSS", "SASS"],
+													 	"randylayne.com/Techdegree-Projects/Web-Style-Guide/"));
+	projects.push(new project(6, 
+														"Interactive Video Player",
+													 	"In this project we take a given template and customize placeholder text and edit styles to match with our own goals and personalities",
+													 	["HTML", "CSS", "Javascript"],
+													 	"randylayne.com/Techdegree-Projects/Interactive-Video-Player/"));
+	projects.push(new project(7, 
+														"SVG Site Update",
+													 	"In this project we take a given template and customize placeholder text and edit styles to match with our own goals and personalities",
+													 	["HTML", "CSS", "SVG", "Design"],
+													 	"randylayne.com/Techdegree-Projects/SVG-Site_Update/"));
+	projects.push(new project(8, 
+														"Game Show App",
+													 	"In this project we take a given template and customize placeholder text and edit styles to match with our own goals and personalities",
+													 	["Javascript", "CSS"],
+													 	"randylayne.com/Techdegree-Projects/Game-Show-App/"));
+	projects.push(new project(9, 
+														"Web App Dashboard",
+													 	"In this project we take a given template and customize placeholder text and edit styles to match with our own goals and personalities",
+													 	["HTML", "CSS", "Javascript", "Widgets", "LocalStorage"],
+													 	"randylayne.com/Techdegree-Projects/Web-App-Dashboard/index.html"));
+	projects.push(new project(10, 
+														"Employee Directory",
+													 	"In this project we take a given template and customize placeholder text and edit styles to match with our own goals and personalities",
+													 	["HTML", "CSS", "Javascript", "AJAX", "JSON"],
+													 	"randylayne.com/Techdegree-Projects/Employee-Directory/"));
+	projects.push(new project(11, 
+														"React Flickr Gallery",
+													 	"In this project we take a given template and customize placeholder text and edit styles to match with our own goals and personalities",
+													 	["HTML", "Javascript", "React", "AJAX", "Node", "JSON", "JSX"],
+													 	"randylayne.com"));
 
 
 
-
-
+}); // ===== end DOMContentLoaded ===
 
 
 
